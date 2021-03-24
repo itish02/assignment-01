434544858ddf6a18730197dabc9098dd573ce932
itish02 <78043075+itish02@users.noreply.github.com>
2021-03-24 13:53:01 +0530

    Vamstar Assignment files

---
+++ script.js
@@ -0,0 +1,9 @@
+const selector1 = document.querySelector(".views-field");
+const selector2 = document.querySelector(".lbl-licitacao");
+const selector3 = document.querySelector(".field-content");
+const selector4 = document.querySelector(".col-sm-6");
+
+console.log(selector1);
+console.log(selector2);
+console.log(selector3);
+console.log(selector4);
